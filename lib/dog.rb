10c168d40fd8b58b9ae94f4a206671251e42d221
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-09 19:03:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 # dog.rb
 class Dog
 end
+
+
+# #### 2. Define `#bark` in `Dog`
+
+# Add an instance method `#bark` to your `Dog` class in `lib/dog.rb` that will puts `"Woof!"`
+
+# #### 3. Define `#sit` in `Dog`
+
+# Add an instance method `#sit` to your `Dog` class in `lib/dog.rb` that will puts `"The Dog is sitting"`.
